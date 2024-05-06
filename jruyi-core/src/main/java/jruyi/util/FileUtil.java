3c273f991be60310a2009a396b769d768fa68993
schuke <xk233qwq@163.com>
2024-05-06 10:16:19 +0800

    优化

--- jruyi-core/src/main/java/jruyi/util/FileUtil.java
+++ jruyi-core/src/main/java/jruyi/util/FileUtil.java
@@ -13,14 +13,15 @@ import java.io.IOException;
 public abstract class FileUtil
 {
     /**
+     * 复制文件
      *
-     * @param from 输入流
-     * @param to 输出流
+     * @param from 输入文件
+     * @param to   输出文件
      * @throws IOException IO 异常
      */
     public static void copy(File from, File to) throws IOException
     {
-        try(var fis = new FileInputStream(from); var fos = new FileOutputStream(to))
+        try (var fis = new FileInputStream(from); var fos = new FileOutputStream(to))
         {
             fis.transferTo(fos);
         }
